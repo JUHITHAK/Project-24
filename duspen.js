9690eb44a5476c21f1f38b553a221c396709ccb3
JUHITHAK <69330522+JUHITHAK@users.noreply.github.com>
2020-08-30 17:36:26 -0700

    Update duspen.js

--- duspen.js
+++ duspen.js
@@ -1,8 +1,8 @@
 class Duspen {
   constructor() {
     sideB = new Ground(950,580,200,20,"blue");
-    sideL = new Ground(860,510,20,100,"blue");
-    sideR = new Ground(1040,510,20,100,"blue");
+    sideL = new Ground(860,525,20,100,"blue");
+    sideR = new Ground(1040,525,20,100,"blue");
   }
   display(){
   sideB.display(); 
